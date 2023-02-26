d607b09e1f8a4081f42cf9cfbd07481ad83b01ac
FLORIAN4600 <53867274+FLORIAN4600@users.noreply.github.com>
2023-02-26 14:06:42 +0100

    Added Trapped Yiorite Ore Block Class

---
+++ src/main/java/fr/florian4600/untitledtroll/block/TrappedYioriteBlock.java
@@ -0,0 +1,64 @@
+package fr.florian4600.untitledtroll.block;
+
+import fr.florian4600.untitledtroll.block.entity.TrappedYioriteOreBlockEntity;
+import fr.florian4600.untitledtroll.utils.YioriteOreUtils;
+import net.minecraft.block.*;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.util.shape.VoxelShapes;
+import net.minecraft.world.BlockView;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
+
+public class TrappedYioriteBlock extends BlockWithEntity {
+
+    public static final VoxelShape SHAPE;
+    public static final VoxelShape BOTTOM_SHAPE;
+    public static final VoxelShape TOP_SHAPE;
+    public static final VoxelShape BARS_SHAPE;
+    public static final VoxelShape OUTLINE_SHAPE;
+
+    public TrappedYioriteBlock(Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    public BlockEntity createBlockEntity(BlockPos pos, BlockState state) {
+        return new TrappedYioriteOreBlockEntity(pos, state);
+    }
+
+    @Override
+    public BlockRenderType getRenderType(BlockState state) {
+        return BlockRenderType.MODEL;
+    }
+
+    @SuppressWarnings("deprecated")
+    @Override
+    public VoxelShape getCollisionShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+        return SHAPE;
+    }
+
+    @SuppressWarnings("deprecated")
+    @Override
+    public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+        return OUTLINE_SHAPE;
+    }
+
+    @Override
+    public void onPlaced(World world, BlockPos pos, BlockState state, @Nullable LivingEntity placer, ItemStack itemStack) {
+        if(world.isClient || placer == null || !placer.isPlayer()) return;
+        YioriteOreUtils.sendText((PlayerEntity) placer, "trapped.placed", state.getBlock().getName());
+    }
+
+    static {
+        SHAPE = VoxelShapes.cuboid(0.0625d, 0d, 0.0625d, 0.9375d, 0.875d, 0.9375d);
+        BOTTOM_SHAPE = VoxelShapes.cuboid(0d, 0d, 0d, 1d, 0.0625d, 1d);
+        TOP_SHAPE = VoxelShapes.cuboid(0.0625d, 0.8125d, 0.0625d, 0.9375d, 0.875d, 0.9375d);
+        BARS_SHAPE = VoxelShapes.cuboid(0.125d, 0.0625d, 0.125d, 0.875d, 0.8125d, 0.875d);
+        OUTLINE_SHAPE = VoxelShapes.union(BOTTOM_SHAPE, BARS_SHAPE, TOP_SHAPE);
+    }
+}
