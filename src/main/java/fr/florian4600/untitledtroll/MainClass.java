491d14d198cafd736ced7e50b87f74a4401f3a1f
FLORIAN4600 <53867274+FLORIAN4600@users.noreply.github.com>
2023-02-26 11:33:39 +0100

    Moved Blocks to a dedicated class

--- src/main/java/fr/florian4600/untitledtroll/MainClass.java
+++ src/main/java/fr/florian4600/untitledtroll/MainClass.java
@@ -1,21 +1,14 @@
 package fr.florian4600.untitledtroll;
 
-import fr.florian4600.untitledtroll.block.YioriteOreBlock;
+import fr.florian4600.untitledtroll.block.UTBlocks;
 import fr.florian4600.untitledtroll.block.entity.YioriteOreBlockEntity;
 import fr.florian4600.untitledtroll.item.UTItems;
 import fr.florian4600.untitledtroll.registry.UTFeaturesRegistry;
-import fr.florian4600.untitledtroll.state.propery.UTProperties;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.entity.FabricBlockEntityTypeBuilder;
-import net.minecraft.block.Block;
-import net.minecraft.block.MapColor;
-import net.minecraft.block.Material;
 import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
-import net.minecraft.sound.BlockSoundGroup;
-import net.minecraft.state.property.Properties;
 import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
@@ -27,18 +20,13 @@ public class MainClass implements ModInitializer {
 	public static final String MOD_ID = "untitledtroll";
 	public static final Logger LOGGER = LoggerFactory.getLogger("untitledtroll");
 
-	public static final Block YIORITE_ORE = new YioriteOreBlock(FabricBlockSettings.of(Material.STONE).dropsNothing().requiresTool().strength(4.3f, 36000f).ticksRandomly().luminance(blockState -> blockState.get(Properties.LIT) ? blockState.get(UTProperties.LIGHT_LEVEL) : 0));
-	public static final Block DEEPSLATE_YIORITE_ORE = new YioriteOreBlock(FabricBlockSettings.copyOf(YIORITE_ORE).mapColor(MapColor.DEEPSLATE_GRAY).strength(6.3f, 46000f).sounds(BlockSoundGroup.DEEPSLATE));
-
-	public static final BlockEntityType<YioriteOreBlockEntity> YIORITE_ORE_ENTITY_TYPE = Registry.register(Registries.BLOCK_ENTITY_TYPE, newId("yiorite_ore"), FabricBlockEntityTypeBuilder.create(YioriteOreBlockEntity::new, YIORITE_ORE, DEEPSLATE_YIORITE_ORE).build());
+	public static final BlockEntityType<YioriteOreBlockEntity> YIORITE_ORE_ENTITY_TYPE = Registry.register(Registries.BLOCK_ENTITY_TYPE, newId("yiorite_ore"), FabricBlockEntityTypeBuilder.create(YioriteOreBlockEntity::new, UTBlocks.YIORITE_ORE, UTBlocks.DEEPSLATE_YIORITE_ORE).build());
 
 	@Override
 	public void onInitialize() {
 		LOGGER.info("Yummy");
 
-		Registry.register(Registries.BLOCK, newId("yiorite_ore"), YIORITE_ORE);
-		Registry.register(Registries.BLOCK, newId("deepslate_yiorite_ore"), DEEPSLATE_YIORITE_ORE);
-
+		UTBlocks.register();
 		UTItems.register();
 		UTFeaturesRegistry.register();
 	}
--- src/main/java/fr/florian4600/untitledtroll/block/UTBlocks.java
+++ src/main/java/fr/florian4600/untitledtroll/block/UTBlocks.java
@@ -1,4 +1,29 @@
 package fr.florian4600.untitledtroll.block;
 
+import fr.florian4600.untitledtroll.MainClass;
+import fr.florian4600.untitledtroll.state.propery.UTProperties;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.Block;
+import net.minecraft.block.MapColor;
+import net.minecraft.block.Material;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.state.property.Properties;
+
 public class UTBlocks {
+    public static final Block YIORITE_ORE = new YioriteOreBlock(FabricBlockSettings.of(Material.STONE).dropsNothing().requiresTool().strength(4.3f, 36000f).ticksRandomly().luminance(blockState -> blockState.get(Properties.LIT) ? blockState.get(UTProperties.LIGHT_LEVEL) : 0));
+    public static final Block DEEPSLATE_YIORITE_ORE = new YioriteOreBlock(FabricBlockSettings.copyOf(YIORITE_ORE).mapColor(MapColor.DEEPSLATE_GRAY).strength(6.3f, 46000f).sounds(BlockSoundGroup.DEEPSLATE));
+    public static final Block TRAPPED_YIORITE_ORE = new Block(FabricBlockSettings.of(Material.METAL).luminance(3).strength(2.4f, 12000f));
+
+    public UTBlocks() {
+
+    }
+
+    public static void register() {
+        Registry.register(Registries.BLOCK, MainClass.newId("yiorite_ore"), YIORITE_ORE);
+        Registry.register(Registries.BLOCK, MainClass.newId("deepslate_yiorite_ore"), DEEPSLATE_YIORITE_ORE);
+        Registry.register(Registries.BLOCK, MainClass.newId("trapped_yiorite_ore"), TRAPPED_YIORITE_ORE);
+        Registry.register(Registries.BLOCK, MainClass.newId("deepslate_trapped_yiorite_ore"), TRAPPED_YIORITE_ORE);
+    }
 }
