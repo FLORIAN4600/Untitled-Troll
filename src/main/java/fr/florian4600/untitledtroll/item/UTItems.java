97efd49ae1f848a459dd80262561e626dc2c193e
FLORIAN4600 <53867274+FLORIAN4600@users.noreply.github.com>
2023-02-26 12:02:20 +0100

    Added Trapped Yiorite Ore Items

--- src/main/java/fr/florian4600/untitledtroll/item/UTItems.java
+++ src/main/java/fr/florian4600/untitledtroll/item/UTItems.java
@@ -5,6 +5,7 @@ import fr.florian4600.untitledtroll.block.UTBlocks;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.item.BlockItem;
 import net.minecraft.item.FoodComponent;
 import net.minecraft.item.Item;
 import net.minecraft.item.ToolMaterials;
@@ -17,6 +18,9 @@ public class UTItems {
     public static final Item YIORITE_ORE = new YioriteOreItemBlock(UTBlocks.YIORITE_ORE, new FabricItemSettings().fireproof().rarity(Rarity.RARE));
     public static final Item DEEPSLATE_YIORITE_ORE = new YioriteOreItemBlock(UTBlocks.DEEPSLATE_YIORITE_ORE, new FabricItemSettings().fireproof().rarity(Rarity.RARE));
 
+    public static final Item TRAPPED_YIORITE_ORE = new BlockItem(UTBlocks.TRAPPED_YIORITE_ORE, new FabricItemSettings().fireproof().rarity(Rarity.EPIC).maxCount(1));
+    public static final Item TRAPPED_DEEPSLATE_YIORITE_ORE = new BlockItem(UTBlocks.TRAPPED_DEEPSLATE_YIORITE_ORE, new FabricItemSettings().fireproof().rarity(Rarity.EPIC).maxCount(1));
+
     public static final Item YIORITE_INGOT = new YioriteIngotItem(ToolMaterials.GOLD, 1, -0.2f,  new FabricItemSettings().fireproof().rarity(Rarity.EPIC).food(new FoodComponent.Builder().alwaysEdible().hunger(7).saturationModifier(1.8f).statusEffect(new StatusEffectInstance(StatusEffects.REGENERATION, 15, 3), 1.0F).statusEffect(new StatusEffectInstance(StatusEffects.BLINDNESS, 25, 1), 0.3F).statusEffect(new StatusEffectInstance(StatusEffects.SLOWNESS, 32, 2), 0.3F).build()));
 
 
@@ -28,8 +32,9 @@ public class UTItems {
         Registry.register(Registries.ITEM, MainClass.newId("yiorite_ore"), YIORITE_ORE);
         Registry.register(Registries.ITEM, MainClass.newId("deepslate_yiorite_ore"), DEEPSLATE_YIORITE_ORE);
 
+        Registry.register(Registries.ITEM, MainClass.newId("trapped_yiorite_ore"), TRAPPED_YIORITE_ORE);
+        Registry.register(Registries.ITEM, MainClass.newId("trapped_deepslate_yiorite_ore"), TRAPPED_DEEPSLATE_YIORITE_ORE);
+
         Registry.register(Registries.ITEM, MainClass.newId("yiorite_ingot"), YIORITE_INGOT);
-
     }
-
 }
