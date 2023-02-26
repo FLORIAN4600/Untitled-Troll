99049f996924793c4b83a52c59ee60cc8f4731ad
FLORIAN4600 <53867274+FLORIAN4600@users.noreply.github.com>
2023-02-26 14:01:43 +0100

    Migrated Entity Types From MainClass to UTBlockEntityTypes and added the Trapped Yiorite Ore Entity Type

--- src/main/java/fr/florian4600/untitledtroll/block/entity/UTBlockEntityTypes.java
+++ src/main/java/fr/florian4600/untitledtroll/block/entity/UTBlockEntityTypes.java
@@ -1,4 +1,18 @@
 package fr.florian4600.untitledtroll.block.entity;
 
+import fr.florian4600.untitledtroll.MainClass;
+import fr.florian4600.untitledtroll.block.UTBlocks;
+import net.fabricmc.fabric.api.object.builder.v1.block.entity.FabricBlockEntityTypeBuilder;
+import net.minecraft.block.entity.BlockEntityType;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+
 public class UTBlockEntityTypes {
+    public static final BlockEntityType<YioriteOreBlockEntity> YIORITE_ORE_ENTITY_TYPE = FabricBlockEntityTypeBuilder.create(YioriteOreBlockEntity::new, UTBlocks.YIORITE_ORE, UTBlocks.DEEPSLATE_YIORITE_ORE).build();
+    public static final BlockEntityType<TrappedYioriteOreBlockEntity> TRAPPED_YIORITE_ORE_ENTITY_TYPE = FabricBlockEntityTypeBuilder.create(TrappedYioriteOreBlockEntity::new, UTBlocks.TRAPPED_YIORITE_ORE, UTBlocks.TRAPPED_DEEPSLATE_YIORITE_ORE).build();
+
+    public static void register() {
+        Registry.register(Registries.BLOCK_ENTITY_TYPE, MainClass.newId("yiorite_ore"), YIORITE_ORE_ENTITY_TYPE);
+        Registry.register(Registries.BLOCK_ENTITY_TYPE, MainClass.newId("trapped_yiorite_ore"), TRAPPED_YIORITE_ORE_ENTITY_TYPE);
+    }
 }
