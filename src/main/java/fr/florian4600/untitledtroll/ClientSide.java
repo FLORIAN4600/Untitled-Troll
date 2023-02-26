d41502d239e692be09f28086f632d16d5ebc44da
FLORIAN4600 <53867274+FLORIAN4600@users.noreply.github.com>
2023-02-26 14:06:15 +0100

    Added ClientSide Rendering for Trapped Yiorite Ore

---
+++ src/main/java/fr/florian4600/untitledtroll/ClientSide.java
@@ -0,0 +1,14 @@
+package fr.florian4600.untitledtroll;
+
+import fr.florian4600.untitledtroll.block.UTBlocks;
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.minecraft.client.render.RenderLayer;
+
+public class ClientSide implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+        BlockRenderLayerMap.INSTANCE.putBlocks(RenderLayer.getTranslucent(), UTBlocks.TRAPPED_YIORITE_ORE, UTBlocks.TRAPPED_DEEPSLATE_YIORITE_ORE);
+    }
+}
