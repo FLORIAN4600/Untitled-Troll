f059c0eb7a594bf60adb98bf8a5e6b9463d9ca2c
FLORIAN4600 <53867274+FLORIAN4600@users.noreply.github.com>
2023-03-05 17:20:55 +0100

    Added the possibility to use the trapped yiorites as storage and to cover them in mud

--- src/main/java/fr/florian4600/untitledtroll/block/TrappedYioriteBlock.java
+++ src/main/java/fr/florian4600/untitledtroll/block/TrappedYioriteBlock.java
@@ -1,16 +1,27 @@
 package fr.florian4600.untitledtroll.block;
 
 import fr.florian4600.untitledtroll.block.entity.TrappedYioriteOreBlockEntity;
+import fr.florian4600.untitledtroll.block.entity.UTBlockEntityTypes;
 import fr.florian4600.untitledtroll.stat.UTStats;
+import fr.florian4600.untitledtroll.state.propery.UTProperties;
 import fr.florian4600.untitledtroll.utils.YioriteOreUtils;
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockRenderType;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.ShapeContext;
 import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.block.entity.BlockEntityTicker;
+import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.ItemEntity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.IntProperty;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.shape.VoxelShape;
@@ -27,6 +38,8 @@ public class TrappedYioriteBlock extends LookableBlock {
     public static final VoxelShape BARS_SHAPE;
     public static final VoxelShape OUTLINE_SHAPE;
 
+    public static final IntProperty MUD_LEVEL;
+
     public TrappedYioriteBlock(Settings settings) {
         super(settings);
     }
@@ -34,6 +47,16 @@ public class TrappedYioriteBlock extends LookableBlock {
     @Override
     public BlockEntity createBlockEntity(BlockPos pos, BlockState state) {
         return new TrappedYioriteOreBlockEntity(pos, state);
+    }
+
+    @SuppressWarnings("deprecated")
+    @Override
+    public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
+        if(world.isClient) return ActionResult.SUCCESS;
+        if (world.getBlockEntity(pos) instanceof TrappedYioriteOreBlockEntity blockEntity) {
+            player.openHandledScreen(blockEntity);
+        }
+        return ActionResult.CONSUME;
     }
 
     @Override
@@ -55,8 +78,13 @@ public class TrappedYioriteBlock extends LookableBlock {
 
     @Override
     public void onPlaced(World world, BlockPos pos, BlockState state, @Nullable LivingEntity placer, ItemStack itemStack) {
-        if(world.isClient || placer == null || !placer.isPlayer() && world.getBlockEntity(pos) == null) return;
-        YioriteOreUtils.sendText((PlayerEntity) placer, "trapped.placed", ((TrappedYioriteOreBlockEntity) world.getBlockEntity(pos)).getCustomName());
+        if(world.isClient()) return;
+        world.setBlockState(pos, state.with(MUD_LEVEL, (itemStack.getNbt() == null || !itemStack.getNbt().contains("MudLevel")) ? 0 : itemStack.getNbt().getInt("MudLevel")));
+        if (world.getBlockEntity(pos) instanceof TrappedYioriteOreBlockEntity blockEntity) {
+            blockEntity.setCustomName(itemStack.getName());
+            if(placer == null || !placer.isPlayer()) return;
+            YioriteOreUtils.sendText((PlayerEntity) placer, "trapped.placed", blockEntity.getCustomName());
+        }
     }
 
     @Override
@@ -65,6 +93,51 @@ public class TrappedYioriteBlock extends LookableBlock {
         PlayerEntity player = (PlayerEntity) entity;
         if(player.isCreative() || player.isSpectator()) return;
         player.incrementStat(UTStats.YIORITE_LOOK_TIME);
+        if(world.getBlockEntity(hitResult.getBlockPos()) instanceof TrappedYioriteOreBlockEntity blockEntity) {
+            blockEntity.setMaxLookingDistance(hitResult.getBlockPos().getManhattanDistance(entity.getBlockPos())*1.33f);
+        }
+    }
+
+    @Nullable
+    @Override
+    public <T extends BlockEntity> BlockEntityTicker<T> getTicker(World world, BlockState state, BlockEntityType<T> type) {
+        return world.isClient ? null : checkType(type, UTBlockEntityTypes.TRAPPED_YIORITE_ORE_ENTITY_TYPE, TrappedYioriteOreBlockEntity::tick);
+    }
+
+    @Override
+    public void onBreak(World world, BlockPos pos, BlockState state, PlayerEntity player) {
+        if(!world.isClient && world.getBlockEntity(pos) instanceof TrappedYioriteOreBlockEntity blockEntity && !(player.isCreative() && blockEntity.isEmpty())) {
+            ItemStack itemStack = new ItemStack(state.getBlock());
+            if(itemStack.getNbt() == null) {
+                itemStack.setNbt(new NbtCompound());
+            }
+            blockEntity.setStackNbt(itemStack);
+            itemStack.setCustomName(blockEntity.getCustomName());
+            itemStack.getNbt().putInt("MudLevel", state.get(MUD_LEVEL));
+            ItemEntity itemEntity = new ItemEntity(world, pos.getX()+0.5, pos.getY()+0.5, pos.getZ()+0.5, itemStack);
+            itemEntity.setToDefaultPickupDelay();
+            world.spawnEntity(itemEntity);
+        }
+        super.onBreak(world, pos, state, player);
+    }
+
+    @SuppressWarnings("deprecated")
+    @Override
+    public boolean hasComparatorOutput(BlockState state) {
+        return true;
+    }
+
+    @SuppressWarnings("deprecated")
+    @Override
+    public int getComparatorOutput(BlockState state, World world, BlockPos pos) {
+        BlockEntity blockEntity = world.getBlockEntity(pos);
+        if(blockEntity == null) return 0;
+        return ((TrappedYioriteOreBlockEntity) blockEntity).getComparatorOutput((TrappedYioriteOreBlockEntity) blockEntity);
+    }
+
+    @Override
+    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+        builder.add(MUD_LEVEL);
     }
 
     static {
@@ -73,5 +146,7 @@ public class TrappedYioriteBlock extends LookableBlock {
         TOP_SHAPE = VoxelShapes.cuboid(0.0625d, 0.8125d, 0.0625d, 0.9375d, 0.875d, 0.9375d);
         BARS_SHAPE = VoxelShapes.cuboid(0.125d, 0.0625d, 0.125d, 0.875d, 0.8125d, 0.875d);
         OUTLINE_SHAPE = VoxelShapes.union(BOTTOM_SHAPE, BARS_SHAPE, TOP_SHAPE);
+
+        MUD_LEVEL = UTProperties.MUD_LEVEL;
     }
 }
--- src/main/java/fr/florian4600/untitledtroll/block/entity/TrappedYioriteOreBlockEntity.java
+++ src/main/java/fr/florian4600/untitledtroll/block/entity/TrappedYioriteOreBlockEntity.java
@@ -1,40 +1,82 @@
 package fr.florian4600.untitledtroll.block.entity;
 
+import fr.florian4600.untitledtroll.MainClass;
 import net.minecraft.block.BlockState;
-import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.block.entity.LootableContainerBlockEntity;
+import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.inventory.Inventories;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
+import net.minecraft.screen.GenericContainerScreenHandler;
+import net.minecraft.screen.ScreenHandler;
+import net.minecraft.screen.ScreenHandlerType;
+import net.minecraft.text.Text;
 import net.minecraft.util.collection.DefaultedList;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
-public class TrappedYioriteOreBlockEntity extends BlockEntity {
+public class TrappedYioriteOreBlockEntity extends LootableContainerBlockEntity {
 
     private DefaultedList<ItemStack> inventory;
-    private String customName;
+    public float maxLookingDistance;
 
     public TrappedYioriteOreBlockEntity(BlockPos pos, BlockState state) {
         super(UTBlockEntityTypes.TRAPPED_YIORITE_ORE_ENTITY_TYPE, pos, state);
-        this.customName = state.getBlock().getName().getString();
+        this.inventory = DefaultedList.ofSize(45, ItemStack.EMPTY);
+        this.maxLookingDistance = -1f;
     }
 
     @Override
     public void readNbt(NbtCompound nbt) {
         super.readNbt(nbt);
         Inventories.readNbt(nbt, inventory);
-        if (nbt.contains("CustomName", 8)) {
-            this.customName = nbt.getString("CustomName");
-        }
     }
 
     @Override
     protected void writeNbt(NbtCompound nbt) {
         super.writeNbt(nbt);
         Inventories.writeNbt(nbt, inventory);
-        nbt.putString("CustomName", this.customName);
     }
 
-    public String getCustomName() {
-        return customName;
+    public static void tick(World world, BlockPos pos, BlockState state, TrappedYioriteOreBlockEntity blockEntity) {
+        blockEntity.setMaxLookingDistance(-1f);
     }
+
+    public float getMaxLookingDistance() {
+        return maxLookingDistance;
+    }
+
+    public void setMaxLookingDistance(float maxLookingDistance) {
+        this.maxLookingDistance = maxLookingDistance;
+    }
+
+    public int getComparatorOutput(TrappedYioriteOreBlockEntity blockEntity) {
+        return Math.round((Math.min(blockEntity.getMaxLookingDistance(), 32f)-32f)*0.4375f);
+    }
+
+    @Override
+    protected Text getContainerName() {
+        return MainClass.getTranslation("container", "trapped_yiorite");
+    }
+
+    @Override
+    protected ScreenHandler createScreenHandler(int syncId, PlayerInventory playerInventory) {
+        return new GenericContainerScreenHandler(ScreenHandlerType.GENERIC_9X5, syncId, playerInventory, this, 5);
+    }
+
+    @Override
+    protected DefaultedList<ItemStack> getInvStackList() {
+        return this.inventory;
+    }
+
+    @Override
+    protected void setInvStackList(DefaultedList<ItemStack> list) {
+        this.inventory = list;
+    }
+
+    @Override
+    public int size() {
+        return 48;
+    }
+
 }
