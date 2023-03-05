176117fb0ad595e0c872aff7061a34f280f73278
FLORIAN4600 <53867274+FLORIAN4600@users.noreply.github.com>
2023-03-05 17:22:44 +0100

    added the MUD_LEVEL property

--- src/main/java/fr/florian4600/untitledtroll/state/propery/UTProperties.java
+++ src/main/java/fr/florian4600/untitledtroll/state/propery/UTProperties.java
@@ -6,6 +6,7 @@ import net.minecraft.state.property.IntProperty;
 public class UTProperties {
     public static final BooleanProperty LOOKED;
     public static final IntProperty LIGHT_LEVEL;
+    public static final IntProperty MUD_LEVEL;
 
     public UTProperties() {
 
@@ -14,5 +15,6 @@ public class UTProperties {
     static {
         LOOKED = BooleanProperty.of("looked");
         LIGHT_LEVEL = IntProperty.of("light_level", 0, 12);
+        MUD_LEVEL = IntProperty.of("mud_level", 0, 3);
     }
 }
